5d7921d5d7577c95584bfc7aebc5d93ad3f8e6fb
pham.ngoc.dong <pham.ngoc.dong@sun-asterisk.com>
2026-03-30 11:20:13 +0700

    remove comment code

--- src/modules/utility/utility.service.ts
+++ src/modules/utility/utility.service.ts
@@ -1,9 +1,9 @@
-import { Injectable, BadRequestException, InternalServerErrorException } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { join } from 'path';
 import { promises as fs } from 'fs';
 import * as bcrypt from 'bcryptjs';
 import { UsersService } from '../users/users.service';
-import { MAX_FILE_SIZE, SALT_ROUNDS } from '../../common/constants';
+import { SALT_ROUNDS } from '../../common/constants';
 import { SharedService } from '../../common/shared.service';
 
 @Injectable()
@@ -14,7 +14,6 @@ export class UtilityService {
   ) {}
 
   async uploadImageForUser(file: Express.Multer.File, user: { userId: number; email: string }) {
-    //this.validateFile(file);
     await this.usersService.checkUserExistOrThrow(user.userId);
 
     try {
@@ -28,21 +27,6 @@ export class UtilityService {
       });
     }
   }
-
-  /*
-  private validateFile(file: Express.Multer.File) {
-    if (!file) {
-      throw new BadRequestException(this.sharedService.getSharedMessage('message.NO_FILE_UPLOADED'));
-    }
-    const allowedTypes = ['image/jpeg', 'image/png', 'image/jpg'];
-    if (!allowedTypes.includes(file.mimetype)) {
-      throw new BadRequestException(this.sharedService.getSharedMessage('message.INVALID_FILE_TYPE'));
-    }
-    if (file.size > MAX_FILE_SIZE * 1024 * 1024) {
-      throw new BadRequestException(this.sharedService.getSharedMessage('message.FILE_TOO_LARGE', { args: { max: MAX_FILE_SIZE } }));
-    }
-  }
-  */
 
   private async uploadImage(file: Express.Multer.File, email: string): Promise<string> {
     const ext = file.originalname.split('.').pop();
